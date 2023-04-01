fa3b5c567e780c6e8eb2fdbde3c8117d73522d8d
Luis Arteaga <luis.arteagaguivin@outlook.com>
2023-04-01 10:43:58 -0500

    Solucion 1er ejercicio

--- ejercicios/soluciones01.py
+++ ejercicios/soluciones01.py
@@ -6,14 +6,16 @@ def ejercicio01():
     y = ['manzana', 'naranjas', 'platano', 'uva']
 
     # 1. mostrar una lista con los divisores de a
-    # hola nose que poner
+    for i in range(1, a + 1):
+        if a % i == 0:
+            print(i)
     # 2. mostrar las palabras de 'y' que su cantidad de letras es par
-    # hola nose que poner
+
     # 3. mostrar la cadena b con un * entre cada letra => c*o*l*o*m*a
-    # hola nose que poner
+
     # 4. filtrar los elemntos de x que son pares
-    # hola nose que poner
+
     # 5. mostrar los elementos de y pero solo las primeras 2 letras de cada palabra
-    # hola nose que poner
+
     # 6. mostrar el duplicado de los elementos x => [2, 4, 10, 12, 16, 18]
     # hola nose que poner cambio
