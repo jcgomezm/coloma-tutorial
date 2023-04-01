88711c2957526b767f60b96fdebbb4b68fcd9617
unknown <jose.reategui.aguirre@gmail.com>
2023-04-01 10:47:49 -0500

    soluciones de guille

--- ejercicios/soluciones01.py
+++ ejercicios/soluciones01.py
@@ -10,11 +10,11 @@ def ejercicio01():
         if a % i == 0:
             print(i)
     # 2. mostrar las palabras de 'y' que su cantidad de letras es par
-
+    # hola nose que poner
     # 3. mostrar la cadena b con un * entre cada letra => c*o*l*o*m*a
-
+    # hola nose que poner
     # 4. filtrar los elemntos de x que son pares
-
+    # hola nose que poner
     # 5. mostrar los elementos de y pero solo las primeras 2 letras de cada palabra
 
     # 6. mostrar el duplicado de los elementos x => [2, 4, 10, 12, 16, 18]
