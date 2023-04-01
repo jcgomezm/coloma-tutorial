bfb000127ca1cde436c733508e0d3be6082ad8c2
Jose Carlos Gomez <jcgmarquez.0@gmail.com>
2023-04-01 09:53:24 -0500

    agregar ejercicios de la clase 1

---
+++ ejercicios/soluciones01.py
@@ -0,0 +1,18 @@
+def ejercicio01():
+    # dados los siguientes valores
+    a = 24
+    b = 'coloma'
+    x = [1, 2, 5, 6, 8, 9]
+    y = ['manzana', 'naranjas', 'platano', 'uva']
+
+    # 1. mostrar una lista con los divisores de a
+
+    # 2. mostrar las palabras de 'y' que su cantidad de letras es par
+
+    # 3. mostrar la cadena b con un * entre cada letra => c*o*l*o*m*a
+
+    # 4. filtrar los elemntos de x que son pares
+
+    # 5. mostrar los elementos de y pero solo las primeras 2 letras de cada palabra
+
+    # 6. mostrar el duplicado de los elementos x => [2, 4, 10, 12, 16, 18]
