96614b70359fe53429c12535232e395c46593b8c
Jose Carlos Gomez <jcgmarquez.0@gmail.com>
2023-04-04 17:29:49 -0500

    ejercicios de funciones

---
+++ ejercicios/ejercicios02.py
@@ -0,0 +1,20 @@
+# 1. crear una funcion que devuelve una lista con los divisores de un numero
+def divisores(a):
+    pass
+
+
+# 2. crear una funcion que reciba n numeros y devuelva el producto de todos
+def producto():
+    return 0
+
+# 3. crear una funcion que pinte el valor de todos los parametros que terminen en A
+# pintar(comida='arroz', lampara='foco', color='azul')
+# 'arroz'
+# 'foco'
+
+# 4. crear una funcion que reciba n parametros y devuelva el producto del primero y el ultimo
+# producto2(4, 2, 3, 1, 5) => 4 * 5 = 20
+
+# 5. crear una funcion que verifique que todos los parametros sean pares
+# producto3(4, 2, 3, 1, 5) => False
+# producto3(4, 2, 6, 8) => True
