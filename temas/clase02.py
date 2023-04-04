1eda9d974cd37bf4e562acb1b705fea325ab2d4e
Jose Carlos Gomez <jcgmarquez.0@gmail.com>
2023-04-04 17:22:38 -0500

    ejercicios agregados durante clase

--- temas/clase02.py
+++ temas/clase02.py
@@ -13,19 +13,30 @@ def con_parametros(nombre, apellido):
     print(f'hola {nombre} {apellido}')
 
 
+def con_parametros_defecto(nombre, apellido='desconocido'):
+    print(f'hola {nombre} {apellido}')
+
+
 def con_pistas(cadena: str):
     print(f'es una cadena: ', cadena, type(cadena))
 
 
 def con_args(*args):
-    print(args)
+    print(f'el primer parametro es {args[0]}')
+    print(f'el ultimo parametro es {args[-1]}')
 
 
+# keyword arguments
 def con_kwargs(**kwargs):
-    print(kwargs)
+    print(f"nombre: {kwargs['nombre']}")
+    if 'apellido' in kwargs:
+        print(f"apellido: {kwargs['apellido']}")
 
 
 def division_decimal(a: int, b: int) -> float:
+    if b == 0:
+        return 'error division entre 0'
+    print('continua la funcion')
     return a / b
 
 
